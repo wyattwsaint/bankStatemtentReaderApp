fda383b14aa9a7ff50f6146d245804acdc088189
Aaron Stout <git@github.com>
2021-10-10 21:30:25 -0400

    loading categories when window loads

--- src/readPDF/Main.java
+++ src/readPDF/Main.java
@@ -5,6 +5,11 @@ import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.table.DefaultTableModel;
 
+import org.apache.commons.collections4.IteratorUtils;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 
@@ -24,13 +29,18 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.text.DecimalFormat;
 import java.text.ParseException;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.swing.border.LineBorder;
@@ -38,9 +48,12 @@ import javax.swing.border.LineBorder;
 public class Main {
 
 	private JFrame frame;
+	private Category catFrame;
 	private JTable tblTransactions;
 	private JLabel lblSummary;
 	DefaultTableModel model;
+	PropertiesConfiguration config;
+	String[][] categoryData;
 
 	/**
 	 * Launch the application.
@@ -69,6 +82,7 @@ public class Main {
 
 	/**
 	 * Initialize the contents of the frame.
+	 * @throws ConfigurationException 
 	 */
 	private void initialize() {
 		frame = new JFrame("Saint Statements");
@@ -137,21 +151,40 @@ public class Main {
 		desktopPane.add(btnNewButton_1);
 		*/
 		
+		
+		try {
+			config = new PropertiesConfiguration("categories.properties");
+			String catProp = config.getProperty("categories").toString();
+			String[] categories = catProp.split(",");
+			
+			categoryData = new String[categories.length][2];
+			int idx = 0;
+			for(String category : categories) {
+				categoryData[idx][0] = category;
+				categoryData[idx][1] = StringUtils.join(config.getStringArray(category), ",");
+				idx++;
+			}
+		
+		} catch (ConfigurationException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		
 		frame.setVisible(true);
+		
+		
 		
 	}
 	
 	public void parseFile(File file) throws IOException, ParseException {
-
+        
 		PDFTextStripper stripper = new PDFTextStripper();
 		String pdfText = stripper.getText(PDDocument.load(file)).toUpperCase().replaceAll("\n", "");
 
 		HashMap<String, Double> summary = new HashMap<>();
 		summary.put("bill", 0.00);
 		summary.put("misc", 0.00);
-		
-		//BufferedWriter csvFile = new BufferedWriter(new FileWriter("csvFile.csv", true));
-		String[] billNames = { "DFAS", "AMAZON", "PRIME VIDEO" };
 		
 		int beginIdx = pdfText.indexOf("CHECKING  ID 0004");
 		int endIdx = pdfText.indexOf("SUMMER PAY SHARES  ID 0005");
@@ -172,8 +205,7 @@ public class Main {
 			amount = DecimalFormat.getNumberInstance().parse(m.group(4)).doubleValue();
 			
 			// Check if transaction is a bill
-			boolean isBill = Arrays.stream(billNames).anyMatch(description::contains);
-			category = isBill ? "bill" : "misc";
+			category = getCategory(description);
 			
 			Object[] newRow = new Object[3];
 			newRow[0] = category;
@@ -196,5 +228,17 @@ public class Main {
 		summaryStr += "</html>";
 		
 		lblSummary.setText(summaryStr);
+	}
+	
+	private String getCategory(String description) {
+		String category = null;
+		
+		for(String[] theCat : categoryData) {
+			if(Arrays.stream(theCat[1].split(",")).anyMatch(description::contains)) {
+				category = theCat[0].toString();
+			}
+		}
+		
+		return (category == null) ? "misc" : category;
 	}
 }
