77db354bfd609e59fcdcb955d63b66c43bf02b66
Wyatt Saint <wmsaint17@gmail.com>
2021-12-05 21:16:44 -0500

    2nd commit

--- src/readPDF/updatedPDFReader.java
+++ src/readPDF/updatedPDFReader.java
@@ -1,9 +1,27 @@
 package readPDF;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
 public class updatedPDFReader {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		
+		stripText();
 
 	}
+	
+	static void stripText() throws IOException  {
+		
+		File file = new File("C:\\Users\\wmsai\\Desktop\\BankStatement.pdf");
+		PDDocument doc = PDDocument.load(file);
+		PDFTextStripper textStripper = new PDFTextStripper();
+		String text = textStripper.getText(doc);
+		System.out.println(text);
+	}
+	
 
 }
